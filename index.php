3ddf697e7c742f43be6b2f1aa3166d3f1d07ecf5
Ivan <onel1n.van@yandex.ru>
2024-09-09 00:24:39 +0300

    headings/paragraphs/styles

--- index.php
+++ index.php
@@ -4,12 +4,21 @@
         <title>HTML</title>
     </head>
     <body>
-        <h1>HTML - это язык разметки, а не язык программирования!!</h1>
-
-        <a href = "https://github.com/VanLin444">Мой GitHub</a>
+        <h1 style = "text-align:center; font-size:250%; color:orange; font-family:Verdana">HTML</h2>
+        <h2>HTML - это язык разметки, а не язык программирования!!</h2>
 
         <img src= "logo_html.png" alt = "HTML logo">  
+        <br>
+        <a href = "https://github.com/VanLin444">Мой GitHub</a>
+        <br>
+
+        <pre style="font-size:200%; color:brown">
+            Птицей Гермеса меня называют, 
+            Cвои крылья пожирая, 
+            Cам себя я укрощаю
+        </pre>
         <footer>
+            <hr>
             <p>© 2024 Моя учебная веб-страница</p>
         </footer>
     </body>
