58cdee9d507538ac7f2563dfcec45978642f8e1a
Ivan <onel1n.van@yandex.ru>
2024-09-08 16:13:54 +0300

    Base HTML

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang = "ru">
+    <head>
+        <title>HTML</title>
+    </head>
+    <body>
+        <h1>HTML - это язык разметки, а не язык программирования!!</h1>
+    </body>
+</html>
