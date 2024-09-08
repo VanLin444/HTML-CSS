292d33901c978c0e52fc9bf067959d17ca61a242
Ivan <onel1n.van@yandex.ru>
2024-09-08 22:49:31 +0300

    h/p/img

--- index.php
+++ index.php
@@ -5,5 +5,12 @@
     </head>
     <body>
         <h1>HTML - это язык разметки, а не язык программирования!!</h1>
+
+        <a href = "https://github.com/VanLin444">Мой GitHub</a>
+
+        <img src= "logo_html.png" alt = "HTML logo">  
+        <footer>
+            <p>© 2024 Моя учебная веб-страница</p>
+        </footer>
     </body>
 </html>
