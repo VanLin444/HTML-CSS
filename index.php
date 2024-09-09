3d7170472d486a727742407d7b9b9f61d887917e
Ivan <onel1n.van@yandex.ru>
2024-09-10 00:41:10 +0300

    Форматирование/Цитирование

--- index.php
+++ index.php
@@ -4,21 +4,76 @@
         <title>HTML</title>
     </head>
     <body>
+        <!-- Заголовок с атрибутом style(исп-ся для добавления стилей к элементу)
+         Атрибуты используются для предоставления дополнительной информации об элементах HTML.
+         -->
         <h1 style = "text-align:center; font-size:250%; color:orange; font-family:Verdana">HTML</h2>
         <h2>HTML - это язык разметки, а не язык программирования!!</h2>
-
+        <!-- Изображение из источника с алтьтернативой вывода, если изображение не загрузилось-->
         <img src= "logo_html.png" alt = "HTML logo">  
+        <!-- <br> Новая строка-->
         <br>
+        <!-- Определяет гиперссылку -->
         <a href = "https://github.com/VanLin444">Мой GitHub</a>
         <br>
-
-        <pre style="font-size:200%; color:brown">
+        <!-- Предварительно отфарматированный текст(Текст сохранияет все пробелы абзацы и т.д) -->
+        <pre style="font-size:200%; color:brown; font-family:Arial, Helvetica, sans-serif">
             Птицей Гермеса меня называют, 
             Cвои крылья пожирая, 
             Cам себя я укрощаю
         </pre>
+
+        <b>Жирный</b>
+
+        <strong>ArmStrong</strong>
+        <br>
+
+        <i>Курсив</i>
+
+        <em>Lupen</em>
+        <br>
+
+        <small>Маленький</small>
+        <br>
+
+        <mark>MARK3</mark>
+        <br>
+
+        <del>Перечёркнуто</del>
+        <br>
+
+        <ins>Подчеркнуто</ins>
+        <br>
+
+        <h4>H<sub>2</sub>O</h4>
+
+        <h5>E = m*c<sup>2</sup></h5>
+
+        <blockquote cite = "https://www.livelib.ru/story/37594-stihotvoreniya-persi-bishi-shelli">
+        Я — Озимандия, великий царь царей. Взгляните на мои деянья и дрожите!
+        </blockquote>
+
+        <h2>Джокер : <q>Я верю, что-то, что тебя не убивает делает тебя страннее!</q></h2>
+
+        <abbr title = "Cascading Style Sheets" style = "font-size:200%"> CSS</abbr>
+        <br>
+
+        <address>
+            Шэрлок Холмс<br>
+            Бейкер-стрит, 221Б, Лондон
+        </address>
+        <br>
+
+        <h3>Ранобэ <cite>Противостояние Святого</cite> Эр Гена, написана в 2009 году.</h3>
+        <br>
+
+        <bdo dir = "rtl" style = "font-size:150%">Иван</bdo>
+        <br>
+
         <footer>
+            <!-- Горизонтальная линия -->
             <hr>
+            <!-- Параграф -->
             <p>© 2024 Моя учебная веб-страница</p>
         </footer>
     </body>
